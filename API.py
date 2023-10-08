ed875b1e9ccb1e419b7336df444bec544ece49ba
Dmitry <kip@gmail.com>
2023-10-08 17:54:23 +0700

    Добавлена функция Получения API и формирование удобного списка для дальнейшей работы с вакансиями

--- API.py
+++ API.py
@@ -1,5 +1,24 @@
 import requests
-import psycopg2
 
-class Api_hh_ru():
-    pass
+
+company_ids = [4970309, 4413765, 6080898, 3302317, 5295281, 3802070, 5156928, 2836019, 46226, 193400]
+
+
+def get_job(company_ids):
+    '''Функция получает с сайта HH вакансии по выбранным компаниям
+     и собирает в удобный список для работы'''
+    responses = []
+    for id in company_ids:
+        url = f'https://api.hh.ru/vacancies?employer_id={id}&per_page=10'
+        employer = requests.get(url).json()
+        for x in employer['items']:
+            salary = 'Не указана' if x['salary'] is None else x['salary']['from']
+            employer_name = 'Не указано' if x['employer'] is None else x['employer']['name']
+            responses.append({'employer_id': id, 'employer_name': employer_name,
+                              'vacancies': {'name': x['name'], 'city': x['area']['name'],
+                                            'salary': salary, 'url': x['alternate_url']}})
+    return responses
+
+# a = get_job(company_ids)
+# for x in a:
+#     print(x)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import psycopg2
+from API import get_job
 
 class DBManager:
     '''Класс для подключения и работы с Базой Данных'''
